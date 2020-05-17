c796ee9acdb05b73f16676c5035d585e7b036bd7
Muhammad Emara <muhammad.emara@gmail.com>
2020-05-17 11:35:50 +0400

    final code with bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+//import 'package:flutter/material.dart';
+//import 'package:flutterlogindemo/main_org.dart';
+//import 'login_signup_page.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutterlogindemo/main_org.dart';
-import 'login_signup_page.dart';
+import 'package:flutterlogindemo/services/authentication.dart';
+import 'package:flutterlogindemo/pages/root_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,12 +14,13 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     // throw UnimplementedError();
     return new MaterialApp(
-      title: 'Flutter Login App',
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: LoginSignupPage(),
-    );
+        title: 'Flutter Login App',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: new RootPage(auth: new Auth())
+        // home: LoginSignupPage(),
+        );
   }
 }
